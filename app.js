f75a114c4c6e66a67d31b048157177bc71065dea
Beruken <bielminok@gmail.com>
2024-05-29 20:26:00 -0300

    WebShop with NoSQL

---
+++ app.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById('665750671783c6cbf21a8240')
+        .then(user => {
+            req.user = new User(user.name, user.email, user.cart, user._id);
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+    app.listen(process.env.PORT || 3000);
+});
---
+++ models/user.js
@@ -0,0 +1,45 @@
+const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb');
+
+const ObjectId = mongodb.ObjectId;
+
+class User {
+    constructor(username, email, cart, id) {
+        this.username = username;
+        this.email = email;
+        this.cart = cart;
+        this._id = id;
+    }
+
+    save() {
+        const db = getDb();
+        return db.collection('users').insertOne(this);
+    }
+
+    addToCart(product) {
+        const cartProductIndex = this.cart.items.findIndex(cp => {
+            return cp.productId.toString() === product._id.toString();
+        });
+        let newQuantity = 1;
+        const updatedCartItems = [...this.cart.items];
+
+        if (cartProductIndex >= 0) {
+            newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+            updatedCartItems[cartProductIndex].quantity = newQuantity;
+        }else{
+            updatedCartItems.push({ productId: new ObjectId(product._id), newQuantity });
+        }
+
+        const updatedCart = { items: updatedCartItems };
+        const db = getDb();
+        return db.collection('users').updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users').findOne({ _id: new ObjectId(userId) });
+        // return db.collection('users').find({ _id: new ObjectId(userId) }).next(); // quando tem certeza de que so há 1 elemento
+    }
+}
+
+module.exports = User;
